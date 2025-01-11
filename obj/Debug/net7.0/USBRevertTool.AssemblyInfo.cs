1de654b67927d886e42243f0a04bd2e353f3aaca
Vimer5410 <gleb.dubinin@rambler.ru>
2025-01-11 03:49:32 +0300

    fixes

--- obj/Debug/net7.0/USBRevertTool.AssemblyInfo.cs
+++ obj/Debug/net7.0/USBRevertTool.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("USBRevertTool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33fc698eb6f180939840814c1a17b1439a9b18f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b56bae311fd31201c932db55a3543c482389a9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("USBRevertTool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("USBRevertTool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
